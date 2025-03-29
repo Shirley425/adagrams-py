e5a062f2d183089b9126501f23636e255628a989
Shirley425 <tanqiaoqiao425@gmail.com>
2025-03-28 22:58:39 -0700

    Update adagrams/game.py
    Co-authored-by: Mikelle <119449245+mikellewade@users.noreply.github.com>

--- adagrams/game.py
+++ adagrams/game.py
@@ -36,7 +36,7 @@ def uses_available_letters(word, letter_bank):
 
 
 def score_word(word):
-    score_dict = {
+    SCORE_DICT = {
         'A': 1, 'E': 1, 'I': 1, 'O': 1, 'U': 1, 'L': 1, 'N': 1, 'R': 1, 'S': 1, 'T': 1,
         'D': 2, 'G': 2,
         'B': 3, 'C': 3, 'M': 3, 'P': 3,
